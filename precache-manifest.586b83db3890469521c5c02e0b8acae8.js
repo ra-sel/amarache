64d37c6a8f68a9d6345ff13b82683127f3245f7c
Md Rasel Ahmed <61163868+ra-sel@users.noreply.github.com>
2020-06-10 00:11:31 +0600

    Add files via upload

---
+++ precache-manifest.586b83db3890469521c5c02e0b8acae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc527805759949b200447cc2d745833",
+    "url": "/amarache/index.html"
+  },
+  {
+    "revision": "f7a5d61bd084fd9bd883",
+    "url": "/amarache/static/css/main.0a315abb.chunk.css"
+  },
+  {
+    "revision": "70aeaea8e9d40482463c",
+    "url": "/amarache/static/js/2.44e846c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amarache/static/js/2.44e846c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a5d61bd084fd9bd883",
+    "url": "/amarache/static/js/main.65c33f75.chunk.js"
+  },
+  {
+    "revision": "d71dd147d840f53fa531",
+    "url": "/amarache/static/js/runtime-main.366e7ae3.js"
+  }
+]);
